44457460d896f7248e9211f15276707f89827cb1
Jonas Bourguignon <jonas.bourguignon@hotmail.com>
2024-05-27 15:01:46 +0200

    Part 2

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+db_name = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "POCTEST"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_name}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
